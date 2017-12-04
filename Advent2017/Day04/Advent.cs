3a104c9c1b7ecad76fa93946a12501e647cbe82a
Uru <johann.vanbeveren@gmail.com>
2017-12-04 21:16:04 +0100

    Version simpler than before

--- Advent2017/Day04/Advent.cs
+++ Advent2017/Day04/Advent.cs
@@ -16,40 +16,9 @@ namespace Advent2017.Day04
 
         public bool GetPassPhraseWithoutAnagramValidity(string passphrase)
         {
-            var words = passphrase.Split(' ').Select(w => new { Word = w }).ToList();
+            var words = passphrase.Split(' ').Select(w => string.Concat(w.OrderBy(c => c)));
 
-            for (int i = 0; i < words.Count; i++)
-            {
-                for (int j = i + 1; j < words.Count; j++)
-                {
-                    if (IsAnagram(words[i].Word, words[j].Word))
-                        return false;
-                }
-            }
-
-            return true;
-        }
-
-        private bool IsAnagram(string word, string word2)
-        {
-            if (word.Length != word2.Length)
-                return false;
-
-            var countDown = word.Length;
-            foreach (var character in word)
-            {
-                for (var i = 0; i < word2.Length; i++)
-                {
-                    if (character == word2[i])
-                    {
-                        countDown--;
-                        word2 = word2.Remove(i,1);
-                        break;
-                    }
-                }
-            }
-
-            return countDown == 0;
+            return !words.All(w => (words.Skip(1)).Contains(w));
         }
     }
 }
