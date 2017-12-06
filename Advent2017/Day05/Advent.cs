814741bbcef9a6736482a1ede7a836fe1026dc5a
Uru <johann.vanbeveren@gmail.com>
2017-12-06 01:13:38 +0100

    Add day 5 first version

---
+++ Advent2017/Day05/Advent.cs
@@ -0,0 +1,41 @@
+﻿using Advent2017.Extension;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Advent2017.Day05
+{
+    public class Advent
+    {
+        public int GetNumberStepToEscapeTheMaze(int[] mazeDetail)
+        {
+            var step = 0;
+            var index = 0;
+            while (index < mazeDetail.Length)
+            {
+                var value = mazeDetail[index];
+                index += value;
+                mazeDetail[index - value]++;
+                step++;
+            }
+
+            return step;
+        }
+
+        public int GetNumberStepToEscapeTheMazeV2(int[] mazeDetail)
+        {
+            var step = 0;
+            var index = 0;
+            while (index < mazeDetail.Length)
+            {
+                var value = mazeDetail[index];
+                index += value;
+                mazeDetail[index - value] += value >= 3 ? -1 : 1;
+                step++;
+            }
+
+            return step;
+        }
+
+    }
+}
---
+++ AdventTest/AdventDay5Should.cs
@@ -0,0 +1,36 @@
+using Advent2017;
+using Advent2017.Day05;
+using NFluent;
+using System.Linq;
+using Xunit;
+
+namespace AdventTest
+{
+    public class AdventDay5Should
+    {
+        private Advent advent;
+
+        public AdventDay5Should()
+        {
+            advent = new Advent();
+        }
+
+        [Theory]
+        [InlineData("0 3 0 1 -3", 5)]
+        public void Escape_The_Maze_In_X_Step(string maze, int stepToExitExpected)
+        {
+            var mazeDetail = maze.Split(' ').Select(x => int.Parse(x)).ToArray();
+            var numberStep = advent.GetNumberStepToEscapeTheMaze(mazeDetail);
+            Check.That(numberStep).Equals(stepToExitExpected);
+        }
+
+        [Theory]
+        [InlineData("0 3 0 1 -3", 10)]
+        public void Escape_The_Maze_In_More_Step(string maze, int stepToExitExpected)
+        {
+            var mazeDetail = maze.Split(' ').Select(x => int.Parse(x)).ToArray();
+            var numberStep = advent.GetNumberStepToEscapeTheMazeV2(mazeDetail);
+            Check.That(numberStep).Equals(stepToExitExpected);
+        }
+    }
+}
