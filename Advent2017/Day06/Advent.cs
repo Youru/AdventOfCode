e2dab91e5f2c4df45a015fc6e372396ee772fcee
Uru <johann.vanbeveren@gmail.com>
2017-12-07 21:06:02 +0100

    Day 06 in progress version

---
+++ Advent2017/Day06/Advent.cs
@@ -0,0 +1,77 @@
+﻿using Advent2017.Extension;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Advent2017.Day06
+{
+    public class Advent
+    {
+        public int GetNumberRedistributionCycle(string maze)
+        {
+            var infiniteLoopDetected = false;
+            var historyBlocks = new Dictionary<string, int>();
+            var blocks = GetBlocks(maze);
+            var loop = 0;
+
+            while (infiniteLoopDetected == false)
+            {
+                var higherBlock = blocks.Max();
+                var indexBlock = blocks.FindIndex(b => b == higherBlock);
+                blocks[indexBlock] = 0;
+                for (var i = 1; i <= higherBlock; i++)
+                {
+                    blocks[(indexBlock + i) % blocks.Count]++;
+                }
+
+                if (historyBlocks.ContainsKey(string.Join(",", blocks))) { 
+                    infiniteLoopDetected = true;
+                }
+                else
+                {
+                    historyBlocks.Add(string.Join(",", blocks), loop);
+                }
+                loop++;
+            }
+
+            return loop;
+
+        }
+
+        public int GetCycleNumberBetweenDuplicate(string maze)
+        {
+            var infiniteLoopDetected = false;
+            var historyBlocks = new Dictionary<string, int>();
+            var blocks = GetBlocks(maze);
+            var loop = 0;
+            var numberCycle = 0;
+
+            while (infiniteLoopDetected == false)
+            {
+                var higherBlock = blocks.Max();
+                var indexBlock = blocks.FindIndex(b => b == higherBlock);
+                blocks[indexBlock] = 0;
+                for (var i = 1; i <= higherBlock; i++)
+                {
+                    blocks[(indexBlock + i) % blocks.Count]++;
+                }
+
+                if (historyBlocks.ContainsKey(string.Join(",", blocks)))
+                {
+                    infiniteLoopDetected = true;
+                    numberCycle = loop - historyBlocks[string.Join(",", blocks)];
+                }
+                else
+                {
+                    historyBlocks.Add(string.Join(",", blocks), loop);
+                }
+                loop++;
+            }
+
+            return numberCycle;
+
+        }
+
+        private List<int> GetBlocks(string input) => (input.Split(' ').ToList().Select(x => int.Parse(x))).ToList();
+    }
+}
---
+++ AdventTest/AdventDay6Should.cs
@@ -0,0 +1,36 @@
+using Advent2017;
+using Advent2017.Day06;
+using NFluent;
+using System.Linq;
+using Xunit;
+
+namespace AdventTest
+{
+    public class AdventDay6Should
+    {
+        private Advent advent;
+
+        public AdventDay6Should()
+        {
+            advent = new Advent();
+        }
+
+        [Theory]
+        [InlineData("0 2 7 0", 5)]
+        public void Get_Cycle_Number_To_Reach_Infinite_Loop(string maze, int resultExpected)
+        {
+            var result = advent.GetNumberRedistributionCycle(maze);
+
+            Check.That(result).Equals(resultExpected);
+        }
+
+        [Theory]
+        [InlineData("0 2 7 0", 4)]
+        public void Get_Cycle_Number_Between_Duplicate(string maze, int resultExpected)
+        {
+            var result = advent.GetCycleNumberBetweenDuplicate(maze);
+
+            Check.That(result).Equals(resultExpected);
+        }        
+    }
+}
