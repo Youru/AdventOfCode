d4774df6c3988b63b53fdd0d1308b0ac980cf8bc
Uru <johann.vanbeveren@gmail.com>
2017-12-08 23:22:24 +0100

    first solution day 8

---
+++ Advent2017/Day08/Advent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2017.Day08
+{
+    public class Advent
+    {
+        public Dictionary<string, int> Register { get; }
+        public int HigherValueDuringProcess { get; private set; } = 0;
+
+        public Advent()
+        {
+            Register = new Dictionary<string, int>();
+        }
+
+        public List<string> GetInstructionParts(string expression)
+        {
+            return expression.Split(' ').ToList();
+        }
+
+        public int ProcessInstructionResult(List<string> instructionParts)
+        {
+            if (!Register.ContainsKey(instructionParts[0])) Register.Add(instructionParts[0], 0);
+            if (!Register.ContainsKey(instructionParts[4])) Register.Add(instructionParts[4], 0);
+
+            if (Compare(instructionParts[5], Register[instructionParts[4]], int.Parse(instructionParts[6])))
+            {
+                var variable = Register[instructionParts[0]];
+                Register[instructionParts[0]] = instructionParts[1] == "inc" ? variable + int.Parse(instructionParts[2]) : variable - int.Parse(instructionParts[2]);
+            }
+
+            HigherValueDuringProcess = HigherValueDuringProcess < Register[instructionParts[0]] ? Register[instructionParts[0]] : HigherValueDuringProcess;
+
+            return Register[instructionParts[0]];
+        }
+
+        private bool Compare(string operatorCompare, int x, int y)
+        {
+            switch (operatorCompare)
+            {
+                case "<": return x < y;
+                case ">": return x > y;
+                case "<=": return x <= y;
+                case ">=": return x >= y;
+                case "!=": return x != y;
+                case "==": return x == y;
+                default: return false;
+            }
+        }
+    }
+}
---
+++ AdventTest/AdventDay8Should.cs
@@ -0,0 +1,57 @@
+using Advent2017;
+using Advent2017.Day08;
+using NFluent;
+using System.Linq;
+using Xunit;
+
+namespace AdventTest
+{
+    public class AdventDay8Should
+    {
+        private Advent advent;
+        public AdventDay8Should()
+        {
+            advent = new Advent();
+        }
+
+        [Theory]
+        [InlineData("b inc 5 if a > 1", 0)]
+        [InlineData("b inc 5 if a == 0", 5)]
+        [InlineData("b dec 5 if a <= 0", -5)]
+        public void Get_Instruction_Result(string instruction, int resultExpected)
+        {
+            var instructionParts = advent.GetInstructionParts(instruction);
+            advent.ProcessInstructionResult(instructionParts);
+        }
+
+        [Fact]
+        public void Get_Processed_Instruction()
+        {
+            var reader = new ReadFile();
+            var lines = reader.GetContentByLine("Day8ExFile");
+
+            foreach (var line in lines)
+            {
+                var instructionParts = advent.GetInstructionParts(line);
+                advent.ProcessInstructionResult(instructionParts);
+            }
+
+            Check.That(advent.Register.Max(r => r.Value)).Equals(1);
+        }
+
+        [Fact]
+        public void Get_Higher_Value_During_Process()
+        {
+            var reader = new ReadFile();
+            var lines = reader.GetContentByLine("Day8ExFile");
+
+            foreach (var line in lines)
+            {
+                var instructionParts = advent.GetInstructionParts(line);
+                advent.ProcessInstructionResult(instructionParts);
+            }
+
+            Check.That(advent.HigherValueDuringProcess).Equals(10);
+        }
+    }
+}
